27d592ecf97417f455a9504427fe23d11393d3db
Hector Sanchez <hola@hectorsanchez.mx>
2020-03-11 01:10:02 -0600

    adds endpoints for balances, owners, transactions

--- index.js
+++ index.js
@@ -2,42 +2,25 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const axios = require('axios');
+const bodyParser = require('body-parser');
+const moment = require('moment');
 
 const BASE_URL = 'https://sandbox.belvo.co/api';
 const USER = '20a17f47-9c76-4001-a8a6-baaacb841324';
 const PASSWORD = 'aOm3LokBYwSHNHP40Oq4gj6qVQ1Hvzyyb@cHoi-QMmOyju9JA7M31QyuiPqq2Q2b';
 
-const PROD_URL = 'https://api.belvo.co/api'
-const PROD_USER = '5b05fe36-cfaa-479f-84d8-6a3e1bc75275';
-const PROD_PASSWORD = 'vOii593Orv0eo5xubv7QrPU-ncoU5LvBb3U5dRDsz-_asRJSXKKipkkF*4PH4DW5'
-
+app.use(bodyParser.json());
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000', 'http://belvo-demo.s3-website-us-east-1.amazonaws.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 
-app.get("/accounts", (req, res, next) => {
-    axios.post(`${BASE_URL}/accounts`, {}, {
-        auth: {
-            username: USER,
-            password: PASSWORD
-        }
-    })
-    .then(response => {
-        console.log(response.data);
-        res.json(response.data);
-    })
-    .catch(error => {
-        console.log(error);
-    });
-});
-
 app.get("/token", (req, res, next) => {
     const data = {
-        id: PASSWORD,
+        id: USER,
         password: PASSWORD,
         scopes: "read_institutions,write_links,read_links,delete_links"
     }
@@ -51,15 +34,97 @@ app.get("/token", (req, res, next) => {
     
     axios.post(`${BASE_URL}/token/`, data, config)
     .then(response => {
-        console.log(response.data.toJSON());
-        res.json(response.data.toJSON());
+        res.json(response.data);
     })
     .catch(error => {
-        console.log('error', error)
-        res.status(405).send({
+        res.status(500).send({
             message: error.toJSON().message
-         });
+        });
     });
 });
+
+app.post("/accounts", (req, res, next) => {
+    axios.post(`${BASE_URL}/accounts/`, {
+        link: req.body.link
+    }, {
+        auth: {
+            username: USER,
+            password: PASSWORD
+        },
+    })
+    .then(response => {
+        res.json(response.data);
+    })
+    .catch(error => {
+        res.status(500).send({
+            message: error.toJSON().message
+        });
+    });
+});
+
+app.post("/transactions", (req, res, next) => {
+    axios.post(`${BASE_URL}/transactions/`, {
+        link: req.body.link,
+        account: req.body.accountId,
+        date_from: '2020-01-01',
+        date_to: moment().format('YYYY-MM-DD')
+    }, {
+        auth: {
+            username: USER,
+            password: PASSWORD
+        }
+    })
+    .then(response => {
+        res.json(response.data);
+    })
+    .catch(error => {
+        res.status(500).send({
+            message: error.toJSON().message
+        });
+    });
+});
+
+app.post("/balances", (req, res, next) => {
+    axios.post(`${BASE_URL}/balances/`, {
+        link: req.body.link,
+        account: req.body.accountId,
+        date_from: '2020-01-01',
+        date_to: moment().format('YYYY-MM-DD')
+    }, {
+        auth: {
+            username: USER,
+            password: PASSWORD
+        }
+    })
+    .then(response => {
+        res.json(response.data);
+    })
+    .catch(error => {
+        res.status(500).send({
+            message: error.toJSON().message
+        });
+    });
+});
+
+app.post("/owners", (req, res, next) => {
+    axios.post(`${BASE_URL}/owners/`, {
+        link: req.body.link
+    }, {
+        auth: {
+            username: USER,
+            password: PASSWORD
+        }
+    })
+    .then(response => {
+        res.json(response.data);
+    })
+    .catch(error => {
+        res.status(500).send({
+            message: error.toJSON().message
+        });
+    });
+});
+
+
 
 app.listen(port, () => console.log(`Server running on port ${port}!`))
